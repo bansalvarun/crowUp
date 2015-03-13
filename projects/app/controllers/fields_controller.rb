068122306d4969cdb38cb61d36c3c6dce2316813
er-varunbansal <er.varunbansal@yahoo.com>
2015-03-14 04:10:45 +0530

    fields made

---
+++ projects/app/controllers/fields_controller.rb
@@ -0,0 +1,41 @@
+class FieldsController < ApplicationController
+	def index
+    	@fields = Field.all
+  	end
+	def new
+		@field = Field.new
+	end
+	def edit
+  		@field = Field.find(params[:id])
+	end
+	def create
+	  @field = Field.new(params.require(:field).permit(:title, :text))
+	 
+	  if @field.save
+   		  redirect_to @field
+   	  else
+      		render 'new'
+      end
+	end
+	def show
+  	  @field = Field.find(params[:id])
+    end
+    def update
+		@field = Field.find(params[:id]) 
+		if @field.update(field_params)
+			redirect_to @field
+		else
+			render 'edit'
+		end
+  	end
+  	def destroy
+	  @field = Field.find(params[:id])
+	  @field.destroy
+  	  redirect_to fields_path
+	end
+	
+	private
+	  def field_params
+	    params.require(:field).permit(:title, :text)
+	  end
+end
